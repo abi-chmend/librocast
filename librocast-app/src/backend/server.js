0be65b2fafea3a2fa4306401a5a1f4ea13c8af20
Kihoon Lee <kihooncs@gmail.com>
2022-04-19 14:40:38 -0700

    NodeJS basic setup

--- librocast-app/src/backend/server.js
+++ librocast-app/src/backend/server.js
@@ -1,3 +1,10 @@
+// run "npm run dev" to run server.js
 import express from "express";
 
+const PORT = 4000;
+
 const app = express();
+
+app.listen(PORT, () => {
+  console.log("HI LIBROCAST");
+});
