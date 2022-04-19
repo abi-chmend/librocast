1c824583ba36c8015c5319c0b4b3ed048fbebef5
Abigail Batinga <abigailbat@Abigails-MacBook-Pro.local>
2022-04-19 12:40:07 -0700

    add react app boilerplate

---
+++ librocast-app/src/utils/firebase.js
@@ -0,0 +1,24 @@
+import firebase from "firebase"
+import "firebase/firestore";
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDqSMPvorj0QIpvZNMRxLlKn2c3EWKQ_dI",
+  authDomain: "librocast-be5ba.firebaseapp.com",
+  projectId: "librocast-be5ba",
+  storageBucket: "librocast-be5ba.appspot.com",
+  messagingSenderId: "785206405414",
+  appId: "1:785206405414:web:6b0db8744d58815c81b9e7",
+  measurementId: "G-W98GX6YLS4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export const db = firebase.firestore();
+
+export default firebase;
