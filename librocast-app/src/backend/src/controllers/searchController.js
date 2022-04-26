d476ef32c8b410dc2596f9775c6424dd052b22c9
Kihoon Lee <kihooncs@gmail.com>
2022-04-25 22:11:14 -0700

    Added goodread_books.csv and implemented book search feature

--- librocast-app/src/backend/src/controllers/searchController.js
+++ librocast-app/src/backend/src/controllers/searchController.js
@@ -1,1 +1,21 @@
-export const search = (req, res) => res.send("search books!!");
+const csv = require("csv-parser");
+const fs = require("fs");
+const filePath = "./src/data/goodreads_books.csv";
+const results = [];
+fs.createReadStream(filePath)
+  .pipe(csv())
+  .on("data", (data) => results.push(data))
+  .on("end", () => {
+    console.log("Successfully parsed the csv file!");
+  });
+
+export const search = (req, res) => {
+  let title = "Jude";
+  let resultText = "Searched for: " + title;
+  let book = results.find((o) => o.title === title);
+  if (book === undefined) {
+    res.send(resultText + " Book not found");
+  } else {
+    res.send(resultText + " isbn: " + book.isbn);
+  }
+};
