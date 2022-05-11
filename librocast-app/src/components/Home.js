582574b0523ff544b157b71f15ba1a27fa0cfe44
Abigail Batinga <41092727+abbybatinga@users.noreply.github.com>
2022-05-10 20:06:22 -0700

    Update Home.js

--- librocast-app/src/components/Home.js
+++ librocast-app/src/components/Home.js
@@ -1,57 +1,118 @@
-import { BrowserRouter } from 'react-router-dom';
 import './App.css'
-import CurrentRead from './CurrentRead.js';
-import HomeGoals from './HomeGoals';
-import Nav from './Nav';
+import dune from './dune.png';
 
 
 function Home() {
-
   function addGoal() {
     let newGoal = document.createElement("div");
 
     let goal = document.getElementById("goal").value;
 
     let goalText = document.createElement("p");
-    goalText.textContent = goal;
+    goalText.textContent = "1. " + goal;
 
-    newGoal.append(goalText);
+    newGoal.appendChild(goalText);
+
+    let goalContainer = document.getElementById("readingGoals");
+
+    goalContainer.appendChild(newGoal);
   }
 
   function addRead() {
     let newRead = document.createElement("div");
 
-    let read = document.getElementById("read");
+    let read = document.getElementById("read").value;
+    let author = document.getElementById("author").value;
+    let currentPage = document.getElementById("pages").value;
     
     let readText = document.createElement("p");
-    readText.textContent = read;
+    readText.textContent = "Title: " + read;
 
-    newRead.append(readText);
+    let authorText = document.createElement("p");
+    authorText.textContent = "Author: " + author;
+
+    let pageText = document.createElement("p");
+    pageText.textContent = "Current page progress: " + currentPage + "/800";
+
+
+    // SAMPLE DATA for now
+    let bookImg = document.createElement("img");
+    bookImg.src = "https://images-na.ssl-images-amazon.com/images/I/81ym3QUd3KL.jpg"
+    bookImg.style.width = "100px";
+    bookImg.style.height = "200px";
+
+    newRead.appendChild(bookImg);
+    newRead.appendChild(readText);
+    newRead.appendChild(authorText);
+    newRead.appendChild(pageText);
+
+    let readsContainer = document.getElementById("readsContainer");
+
+    newRead.style.margin = "40px";
+
+    readsContainer.appendChild(newRead);
   }
 
 
   return (
-    
-    <div id="home">
+    <div id="App">
+
+      <h1>Welcome back, Name!</h1>
       
-      <h1>Welcome back, Abigail</h1>
 
       <div id="readingGoals">
         <h1>Today's Reading Goals</h1>
         <hr></hr>
+
+        <h3>Enter your goal</h3>
         <input type="text" id="goal"></input>
+        
         <button id="addGoalBtn" onClick={addGoal}>
           Add Goal
         </button>
+
+        <div id="goalsContainer">
+          <h3>Goals:</h3>
+        </div>
       </div>
 
 
       <div id="currentRead">
         <h1>Currently reading...</h1>
+
+        <h3>Enter your current read</h3>
+
+
+        <div>
+          <h3>Title: </h3>
+          <input type="text" id="read"></input>
+        </div>
+        
+
+        <div>
+          <h3>Author: </h3>
+          <input type="text" id="author"></input>
+        </div>
+
+
+        <div>
+          <h3>Current Page: </h3>
+          <input type="text" id="pages"></input>
+        </div>
+
+
+
         <button id="addReadBtn" onClick={addRead}>
           Add current read
         </button>
-        <input type="text" id="read"></input>
+
+
+        
+
+
+        <div id="readsContainer">
+          <h3>Current Read:</h3>
+        </div>
       </div>
 
       
