90581daa9f5ebc14d5a075de684f855c0a762bb4
jchao03 <jchao03@uw.edu>
2022-05-11 14:31:08 -0700

    clean leftover comments and empty lines in app

--- librocast-app/src/App.js
+++ librocast-app/src/App.js
@@ -1,6 +1,5 @@
 import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
 import SignUp from './signUp'
-import LogIn from './logIn'
 import Home from './components/Home'
 import { useAuthContext } from './useAuthContext'
 import './components/App.css'
@@ -9,26 +8,12 @@ import Auth from './components/Auth'
 import Onboard from './components/Onboarding'
 import Nav from './components/Nav';
 import Profile from './utils/profile/Profile';
-import {useState} from 'react';
 
 function App() {
   const { user, authIsReady } = useAuthContext()
 
   return (
     <div className="App" >
-      {/*}
-      { authIsReady && (
-        <BrowserRouter>
-          <Routes>
-              <Route path="/" />
-              <Route path="/signup" element={<SignUp />} />
-              <Route path="/login" element={<LogIn />} />
-          </Routes>
-        </BrowserRouter>
-      )}
-      */}
-
-
       {authIsReady &&
       <BrowserRouter>
         {user ? <Nav></Nav> : null}
@@ -42,9 +27,6 @@ function App() {
             <Route path="/search" element={<Search />}/>
         </Routes>
       </BrowserRouter>}
-
-       
-
     </div>
   )}
 
