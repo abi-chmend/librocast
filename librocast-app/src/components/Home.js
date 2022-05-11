e5c1833c35d54a21d22b2c1d59ff28ce6c5d32ca
jchao03 <jchao03@uw.edu>
2022-05-11 15:23:01 -0700

    update home displayed information based on logged in user

--- librocast-app/src/components/Home.js
+++ librocast-app/src/components/Home.js
@@ -1,25 +1,35 @@
 import './App.css'
-
+import {GetUserProfile} from '../backend/Query'
 
 function Home() {
+  const userProfile = GetUserProfile()
   return (
     <div id="App">
-
-      <h1>Welcome back, Abigale!</h1>
-      
-
-      <div id="readingGoals">
-
-      </div>
-
-
-      <div id="currentRead">
-
-      </div>
-
-      
+      {userProfile.map((profile) => (
+          <DisplayHome
+              displayName={profile.data.displayName}
+              goals={profile.data.goals}
+              currentBook={profile.data.book_reading}/>
+      ))}
     </div>
   )
+}
+
+function DisplayHome(props) {
+    return(
+        <div className="homeWrapper">
+            <h1>Welcome Back, {props.displayName}</h1>
+            <div id="readingGoals">
+                {props.goals.map((goal, index) => (
+                    <p>Goal: {goal["goal_id"]} Progress: {goal["progress"]}</p>
+                ))}
+            </div>
+
+            <div id="currentRead">
+                <h2>Currently Reading: {props.currentBook}</h2>
+            </div>
+        </div>
+    );
 }
 
 export default Home; 
