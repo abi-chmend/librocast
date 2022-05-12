aab4a0e261f18f2b9558b4443f5fbd049d41b736
jchao03 <jchao03@uw.edu>
2022-05-11 20:05:01 -0700

    fixed onboarding warnings

--- librocast-app/src/components/Onboarding.js
+++ librocast-app/src/components/Onboarding.js
@@ -18,7 +18,7 @@ function Onboard() {
         setCount(count+1);
 
         let question = document.getElementById("question");
-        if (count == 0) {
+        if (count === 0) {
             let midBtn = document.getElementById("mid");
             midBtn.style.backgroundColor = "#0F4B4E";
 
@@ -27,7 +27,7 @@ function Onboard() {
 
             question.textContent = "What are your top 3 goals?";
 
-        } else if (count == 1) {
+        } else if (count === 1) {
            const goals = document.getElementById("response").value.split(',');
 
            goals.forEach((goal) => {
@@ -42,7 +42,7 @@ function Onboard() {
 
 
             question.textContent = "What are your top 3 genres?";
-        } else if (count == 2) {
+        } else if (count === 2) {
             // Skeleton for associating favorite genre to new users. To be revised
             // based on whether genres should be implemented as dropdown
             /**
@@ -67,7 +67,7 @@ function Onboard() {
             <h1 id="question">What is your name?</h1>
             <input type="text" id="response" name="response"></input>
             <div>
-                <img id="nextBtn" src={next} onClick={nextClicked}></img>
+                <img id="nextBtn" src={next} onClick={nextClicked} alt="View next onboarding question"></img>
             </div>
             <span id="left" className="dot"></span>
             <span id="mid" className="dot"></span>
