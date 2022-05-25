a53344c87f28352adec73c5f550701c73553adbf
Kihoon Lee <65799897+kihooncs@users.noreply.github.com>
2022-05-25 00:29:06 -0700

    Delete searchRouter.js

--- librocast-app/src/backend/src/routers/searchRouter.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { bookSearch } from "../controllers/searchController";
-
-const searchRouter = express.Router();
-
-searchRouter.get("/", bookSearch);
-
-export default searchRouter;
