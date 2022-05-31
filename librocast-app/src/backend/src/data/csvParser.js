8a33dfccbfb3608b1700b9a377fad1c1ae2abad8
Kihoon Lee <65799897+kihooncs@users.noreply.github.com>
2022-05-31 16:25:43 -0700

    Update csvParser.js

--- librocast-app/src/backend/src/data/csvParser.js
+++ librocast-app/src/backend/src/data/csvParser.js
@@ -2,12 +2,14 @@ const csv = require("csv-parser");
 const fs = require("fs");
 const filePath = "./src/data/goodreads_books.csv";
 const results = [];
-fs.createReadStream(filePath)
-  .pipe(csv())
-  .on("data", (data) => results.push(data))
-  .on("end", () => {
-    console.log("Successfully parsed the csv file!");
-    console.log("results array has " + results.length + " books.");
-  });
+const main = async () => {
+  fs.createReadStream(filePath)
+    .pipe(csv())
+    .on("data", (data) => results.push(data))
+    .on("end", () => {
+      //console.log("Successfully parsed the csv file!");
+      //console.log("results array has " + results.length + " books.");
+    });
+};
 
 export default results;
