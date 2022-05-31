d062eeb737238f6538489a491ac14cd3be8903c5
Abigail Batinga <abigailbat@Abigails-MacBook-Pro-2.local>
2022-05-31 15:01:51 -0700

    add feed

---
+++ librocast-app/src/components/Feed.js
@@ -0,0 +1,28 @@
+import './feed.css';
+
+export default function Feed() {
+  return (
+    <div className="userFeed">
+      <h1>See what's new</h1>
+      <h1>Friend Feed</h1>
+    </div>
+  )
+}
+
+function FeedUpdates() {
+  return (
+    <div className="updates">
+
+    </div>
+  )
+}
+
+function FriendFeed() {
+
+  return (
+    <div className="friendFeed">
+      
+    </div>
+  )
+
+}
